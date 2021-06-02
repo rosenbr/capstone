88ef2d3852a1a94939fac1804cdec130d1ca426f
Richard Rosenberg <rosenbr73@gmail.com>
2021-06-02 12:08:11 -0700

    base routes added to base landing page

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.express = {
+    recipes: require("./recipes")
+}
--- server.js
+++ server.js
@@ -5,10 +5,30 @@ const app = express();
 const methodOverride = require("method-override");
 
 app.get("/", function(req, res){
-    res.render("/");
+    res.render("../views/index");
 });
 
-app.set("view enjine", "ejs");
+app.get("/home", function(req, res){
+    res.render("../views/home");
+});
+
+app.get("/browse", function(req, res){
+    res.render("../views/browse");
+});
+
+app.get("/catagory", function(req, res){
+    res.render("../views/catagory");
+});
+
+app.get("/profile", function(req, res){
+    res.render("../views/profile");
+});
+
+app.get("/recipe", function(req, res){
+    res.render("../views/recipe");
+});
+
+app.set("view engine", "ejs");
 
 
 app.listen(3000, function(){
